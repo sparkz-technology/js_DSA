44129d4309fccdda212457cc10609ed307e8c3c6
sutharsan.webdev <sutharsan.webdev@gmail.com>
2024-10-09 16:34:44 +0000

    merge sort

---
+++ sort/mergeSort.js
@@ -0,0 +1,32 @@
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+let numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+console.log(mergeSort(numbers));
